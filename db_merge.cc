4799e7a38b36a56306b91155af77bcd0850247ce
Nikolay Mihaylov <nmmm@nmmm.nu>
2016-02-29 17:49:31 +0200

    make db_merge get advantage of single and two file merge, using DiskTale and DualTable

--- db_merge.cc
+++ db_merge.cc
@@ -1,6 +1,8 @@
 #include "lsmtable.h"
 #include "argtableloader.h"
 
+#include "dualtable.h"
+
 #include <unistd.h>	// access
 
 static void printUsage(const char *name){
@@ -19,6 +21,15 @@ static void printUsage(const char *name){
 
 inline bool fileExists(const StringRef& name) {
 	return access(name.data(), F_OK) != -1;
+}
+
+template <class TABLE>
+int merge(const TABLE &table, const char *output, bool const keepInvalid, bool const keepTombstones){
+	DiskFile df = DiskFile(output);
+
+	df.createFromList(table, keepInvalid, keepTombstones);
+
+	return 0;
 }
 
 int main(int argc, char **argv){
@@ -52,12 +63,43 @@ int main(int argc, char **argv){
 		return 2;
 	}
 
-	ArgTableLoader al { pathc, path };
+	if (pathc == 1){
+		const char *filename = path[0];
 
-	LSMTable<ArgTableLoader::container_type> mtlist( *al );
+		DiskTable table;
+		table.open(filename);
 
-	DiskFile df = DiskFile(output);
+		printf("Merging (cleanup) single file...\n");
+		printf("\t%s\n", filename);
 
-	df.createFromList(mtlist, keepInvalid, keepTombstones);
+		return merge(table, output, keepInvalid, keepTombstones);
+
+	}else if (pathc == 2){
+		const char *filename1 = path[0];
+		const char *filename2 = path[1];
+
+		DiskTable table1;
+		table1.open(filename1);
+
+		DiskTable table2;
+		table2.open(filename2);
+
+		DualTable<DiskTable, DiskTable> table(table1, table2);
+
+		printf("Merging two files...\n");
+		printf("\t%s\n", filename1);
+		printf("\t%s\n", filename2);
+
+		return merge(table, output, keepInvalid, keepTombstones);
+
+	}else{
+		ArgTableLoader al { pathc, path };
+
+		LSMTable<ArgTableLoader::container_type> table( *al );
+
+		printf("Merging multiple files...\n");
+
+		return merge(table, output, keepInvalid, keepTombstones);
+	}
 }
 
