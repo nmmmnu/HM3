9fd55cae8af520c8f631e0bc0eae2f998ad5a138
Nikolay Mihaylov <nmmm@nmmm.nu>
2016-07-23 12:37:39 +0300

    add db_lsmheat

--- hm3/arraysearch/jumplocator.h
+++ hm3/arraysearch/jumplocator.h
@@ -14,6 +14,8 @@ namespace arraysearch{
  *	- underline list does not need to be immutable.
  *	- gradual prefetch - it does not need to prefetch whole DiskTable.
  *
+ * Check the stats at the end of the file.
+ *
  */
 
 class JumpLocator{
@@ -22,15 +24,6 @@ public:
 
 	static constexpr my_size_type	MINIMAL_JUMP		= 1000 * 1000;
 	static constexpr my_size_type	DEFAULT_JUMPS		= 500;
-
-	/*
-	LSM on 6 huge files:
-	PartitionSearch<BinarySearch>
-	500		7G/228G		1:12		0:025
-
-	SimpleSearch<JumpSearch>
-	500		7G/228G		0:54		0:025
-	*/
 
 public:
 	JumpLocator(my_size_type const jumps = DEFAULT_JUMPS) : jumps_(jumps){}
@@ -58,6 +51,36 @@ private:
 // ==============================
 
 #include "jumplocator_impl.h"
+
+#endif
+
+#if 0
+
+LSM on 6 huge tables:
+==============================
+Class				Disk	Parts		VM Stats	Prefetch/Heat	Search
+PartitionSearch<BinarySearch>	RAID5	500		7G/228G		1:12		0:02.5
+SimpleSearch<JumpSearch>	RAID5	500		7G/228G		0:54		0:02.5
+SimpleSearch<JumpSearch>	SSD	500		170M/228G	0:00.8		0:00.5
+SimpleSearch<JumpSearch>	USB2	500		170M/228G	0:05		0:00.5
+
+LSM on 230 tables ~8M keys each:
+==============================
+Class				Disk	Parts		VM Stats	Prefetch/Heat	Search
+PartitionSearch<BinarySearch>	RAID5	100		242M/128G	3:45		0:14
+PartitionSearch<BinarySearch>	RAID5	500		912M/128G	8:15		0:09
+SimpleSearch<JumpSearch>	RAID5	500		6G/128G		1:00		0:20
+SimpleSearch<JumpSearch>	SSD	500		230M/128G	0:01		0:01
+
+==============================
+Disks are:
+	RAID5	Western Digital Red,
+			4 x 4 TB + 1 x 6 TB,
+			connection over SATA3
+	SSD	Samsung 850 EVO, 250 GB,
+			connection over SATA3
+	USB2	Samsung 850 EVO, 250 GB,
+			connection over USB2
 
 #endif
 
---
+++ hm3/main/db_lsmheat.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include "lsmtable.h"
+
+#include "tableloader/directorytableloader.h"
+
+static void printUsage(const char *cmd){
+	std::cout
+		<< "This program retrieve first and last element of each disktable from the LSM tree."	<< std::endl
+		<< "It creates 'hotspots' into OS cache, so next searches to be fast."			<< std::endl
+		<< std::endl
+
+		<< "Usage:"	<< std::endl
+		<< "\t"		<< cmd	<< "[lsm_path] - preload / heat lsm_path"	<< std::endl
+
+		<< "\t\tPath names must be written without extention"		<< std::endl
+		<< "\t\tExample 'directory/file.*'"				<< std::endl
+		<< "\t\tExample 'directory/file.db'"				<< std::endl
+
+		<< std::endl;
+}
+
+int main(int argc, char **argv){
+	if (argc <= 1){
+		printUsage(argv[0]);
+		return 1;
+	}
+
+	// =======================
+
+	const auto path		= argv[1];
+
+	// =======================
+
+	using DirectoryTableLoader = hm3::tableloader::DirectoryTableLoader;
+	using MyLSMTable = hm3::LSMTable<DirectoryTableLoader::container_type>;
+
+	DirectoryTableLoader dl{ path };
+	MyLSMTable mtlist(*dl);
+
+	// =======================
+
+	static const char frst[] = { (char) 0x01, '\0' };
+	static const char last[] = { (char) 0xFF, '\0' };
+
+	mtlist.getIterator(frst);
+	mtlist.getIterator(last);
+
+	return 0;
+}
+
