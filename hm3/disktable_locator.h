9794a4286a0c6f21f3c0d95a71a720cf378f798d
Nikolay Mihaylov <nmmm@nmmm.nu>
2016-07-23 10:40:19 +0300

    remove PartitionSearch

--- hm3/disktable_locator.h
+++ hm3/disktable_locator.h
@@ -1,18 +1,10 @@
 
 #define DISK_TABLE_LOCATOR	'J'
-//#define DISK_TABLE_LOCATOR	'P'
 //#define DISK_TABLE_LOCATOR	'B'
 
 // ============================================
 
-#if DISK_TABLE_LOCATOR == 'P'
-
-#include "arraysearch/partitionsearch.h"
-#include "arraysearch/binarylocator.h"
-
-#define DISK_TABLE_LOCATOR_SEARCH	arraysearch::PartitionSearch<arraysearch::BinaryLocator>
-
-#elif DISK_TABLE_LOCATOR == 'J'
+#if DISK_TABLE_LOCATOR == 'J'
 
 #include "arraysearch/simplesearch.h"
 #include "arraysearch/jumplocator.h"
